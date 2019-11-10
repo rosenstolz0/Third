3bac5f89b821085bc11169bd886170f291874002
Aika <aiperi.nurlanova7@gmail.com>
2019-11-10 15:07:01 -0600

    adding some methods

--- src/Third.java
+++ src/Third.java
@@ -1,5 +1,7 @@
 public class Third {
     public static void main(String[] args) {
         System.out.println("lllllll");
+        System.out.println("mmmmmmm");
+        System.out.println("kkkkkkkk");
     }
 }
